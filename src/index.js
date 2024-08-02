4f437abadc0b2c91dd1f8fc17ebb04f88395f6c3
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2024-08-02 16:12:39 +0300

    feat: complete the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let str = Math.abs(n).toString()
+    let newString = ''
+    for (let i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return +newString;
 }
